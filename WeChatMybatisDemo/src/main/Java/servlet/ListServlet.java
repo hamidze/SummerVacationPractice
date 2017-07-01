bbb5e80897b6085353dd28c81b35e72567c8b851
Thpffcj <1441732331@qq.com>
2017-07-01 21:51:26 +0800

    添加一个Mybatis项目

---
+++ WeChatMybatisDemo/src/main/Java/servlet/ListServlet.java
@@ -0,0 +1,24 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 列表页面初始化控制
+ * Created by Thpffcj on 2017/7/1.
+ */
+public class ListServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/jsp/back/list.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
