814f4875efa53611adca6ae07ff425715e41caae
Thpffcj <1441732331@qq.com>
2017-07-16 21:25:46 +0800

    继续WeChatMybatisDemo项目

--- WeChatMybatisDemo/src/main/Java/servlet/ListServlet.java
+++ WeChatMybatisDemo/src/main/Java/servlet/ListServlet.java
@@ -1,10 +1,15 @@
 package servlet;
 
+import bean.Message;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 列表页面初始化控制
@@ -14,6 +19,28 @@ public class ListServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/micro_message","root","000000");
+            String sql = "SELECT id, command, description, content FROM message";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+            List<Message> messageList = new ArrayList<Message>();
+            while(rs.next()){
+                Message message = new Message();
+                message.setId(rs.getString("ID"));
+                message.setCommand(rs.getString("COMMAND"));
+                message.setContent(rs.getString("CONTENT"));
+                message.setDescription(rs.getString("DESCRIPTION"));
+                messageList.add(message);
+            }
+//            System.out.println(messageList.get(0).getDescription());
+            req.setAttribute("messageList", messageList);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         req.getRequestDispatcher("/WEB-INF/jsp/back/list.jsp").forward(req, resp);
     }
 
