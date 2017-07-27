62510efd21021e94d2df149504485d0032c7383d
Thpffcj <1441732331@qq.com>
2017-07-27 10:55:12 +0800

    Add the project attribute configuration and controller usage

--- SpringBootDemo/src/main/java/cn/edu/nju/HelloController.java
+++ SpringBootDemo/src/main/java/cn/edu/nju/HelloController.java
@@ -1,17 +1,23 @@
 package cn.edu.nju;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Thpffcj on 2017/7/26.
  */
 @RestController
+@RequestMapping("hello")
 public class HelloController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String say() {
-        return "Hello Spring Boot";
+    @Autowired
+    private GirlProperties girlProperties;
+
+//    @RequestMapping(value = {"/say/{id}", "/hi"}, method = RequestMethod.GET)
+    @GetMapping(value = "/say")
+    public String say(@PathVariable("id") Integer id) {
+//        return girlProperties.getCupSize();
+        return "id: " + id;
     }
 }
