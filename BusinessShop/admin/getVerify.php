ef736ac4cdc66454b0919ed5694049cad19cba33
Thpffcj <1441732331@qq.com>
2017-09-03 14:58:40 +0800

    添加所需静态资源,添加登录验证码操作

---
+++ BusinessShop/admin/getVerify.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Thpffcj
+ * Date: 2017/9/3
+ * Time: 10:59
+ */
+require_once '../include.php';
+verifyImage();
---
+++ BusinessShop/lib/common.func.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Thpffcj
+ * Date: 2017/9/3
+ * Time: 10:09
+ */
