363293d74ec053e832de10dee1f2873d8c166e14
Thpffcj <1441732331@qq.com>
2017-09-06 16:49:03 +0800

    添加图片上传功能

--- BusinessShop/lib/string.func.php
+++ BusinessShop/lib/string.func.php
@@ -26,3 +26,24 @@ function buildRandomString($type=1,$length=4){
     $chars = str_shuffle ( $chars );
     return substr ( $chars, 0, $length );
 }
+
+/**
+ * 生成唯一字符串
+ * @return string
+ */
+function getUniName(){
+    return md5(uniqid(microtime(true),true));
+}
+
+/**
+ * 得到文件的扩展名
+ * @param string $filename
+ * @return string
+ */
+function getExt($filename){
+//    return strtolower(end(explode(".",$filename)));
+    $array=explode(".", $filename);
+    $str=end($array);
+    $extstr=strtolower($str);
+    return $extstr;
+}
